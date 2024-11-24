c104967e40d967a521f8878312bdfbb30b16ee8d
naufalulwan <naufalulwan63@gmail.com>
2024-11-24 23:37:42 +0700

    Merubah response dari auth

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -15,31 +15,31 @@ func AuthHandler(username, password, deviceId, deviceToken, ssn string, isInfoSa
 
 	err = config.DB.Model(models.User{}).Where("username = ?", username).Take(&res).Error
 	if err != nil {
-		return "", "", res, err
+		return "", "", res, fmt.Errorf("username atau password salah, silahkan cek kembali")
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(res.Password), []byte(password))
 	if err != nil && errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-		return "", "", res, err
+		return "", "", res, fmt.Errorf("username atau password salah, silahkan cek kembali")
 	}
 
 	if res.Ssn == "" {
 		err = config.DB.Model(models.User{}).Where("id = ?", res.ID).Update("ssn", ssn).Error
 		if err != nil {
-			return "", "", res, err
+			return "", "", res, fmt.Errorf("terjadi kesalahan saat menyimpan data SSN")
 		}
 	} else if res.Ssn != ssn {
-		return "", "", res, fmt.Errorf("your ssn is already registered in another account")
+		return "", "", res, fmt.Errorf("akun anda sudah digunakan pada device lain, silahkan hubungi ke admin")
 	}
 
 	err = config.DB.Model(models.User{}).Where("id = ?", res.ID).Update("device_id", deviceId).Update("save_login", isInfoSave).Update("device_token", deviceToken).Update("last_login", time.Now()).Error
 	if err != nil {
-		return "", "", res, err
+		return "", "", res, fmt.Errorf("terjadi kesalahan saat menyimpan data device ID, device token, dan last login")
 	}
 
 	accessToken, refreshToken, err := GenerateToken(res.ID, res.Username, res.DeviceId, res.Email, res.Level)
 	if err != nil {
-		return "", "", res, err
+		return "", "", res, fmt.Errorf("terjadi kesalahan saat membuat token")
 	}
 
 	res.Password = ""
